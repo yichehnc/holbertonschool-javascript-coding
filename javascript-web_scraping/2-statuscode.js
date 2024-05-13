c12b52c96dfe6470033f5c60a67f638124a0fcf8
yichehnc <yichenc2017@gmail.com>
2024-05-14 07:39:51 +1000

    feat: Write a script that display the status code of a GET request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require("request");
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.err(err);
+    return;
+  }
+  if (response) {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
