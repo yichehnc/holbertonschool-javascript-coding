31a3687bcbd749c50093dff95a22dae43f9f6c86
yichehnc <yichenc2017@gmail.com>
2024-05-14 07:17:07 +1000

    feat: Write a script that reads and prints the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require("fs");
+const filepath = process.argv[2];
+
+fs.readFile(filepath, "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
