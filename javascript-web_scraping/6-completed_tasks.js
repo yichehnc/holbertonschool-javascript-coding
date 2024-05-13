7e49dcc6c1a51d955b7e364b918d7c3a044e51e9
yichehnc <yichenc2017@gmail.com>
2024-05-14 08:18:47 +1000

    feat: Write a script that computes the number of tasks completed by user id

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require("request");
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const todos = JSON.parse(body);
+  const count = {};
+  for (const todo of todos) {
+    if (todo.completed) {
+      if (count[todo.userID]) {
+        count[todo.userID] = count[todo.userID] + 1;
+      } else {
+        count[todo.userID] = 1;
+      }
+    }
+  }
+
+  const sortedCount = Object.entries(count).sort((a, b) => a[0] - b[0]);
+  const formattedOutput = {};
+  for (const [userId, taskCount] of sortedCount) {
+    formattedOutput[userId] = taskCount;
+  }
+  console.log(formattedOutput);
+});
