a174825c66c4cf3aff3b04cdebca56f1df48cc37
yichehnc <yichenc2017@gmail.com>
2024-05-15 09:52:24 +1000

    fix: change writeFile function to correct error

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const fs = require("fs");
-const filePath = process.argv[2];
+const fs = require('fs');
+const file = process.argv[2];
 const string = process.argv[3];
-
-fs.writefile(filePath, String(string), (err) => {
+fs.writeFile(file, String(string), (err) => {
   if (err) {
     console.log(err);
   }
