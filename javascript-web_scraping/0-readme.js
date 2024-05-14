d3510a2b0dbfc41525fd519ef05d4c77c30beb0a
yichehnc <yichenc2017@gmail.com>
2024-05-15 09:41:05 +1000

    style: semistandard style

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 const filepath = process.argv[2];
 
-fs.readFile(filepath, "utf-8", (err, data) => {
+fs.readFile(filepath, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
     return;
