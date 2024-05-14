6c67b1a3d541082b7e6010c7590122ffa502c03f
yichehnc <yichenc2017@gmail.com>
2024-05-15 09:39:38 +1000

    style: semistandard style

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const request = require("request");
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
@@ -9,7 +9,7 @@ request.get(url, (err, response, body) => {
     console.error(err);
     return;
   }
-  fs.writeFile(filePath, body, "utf-8", (err) => {
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
     if (err) {
       console.error(err);
     }
