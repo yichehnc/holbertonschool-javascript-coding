97a9a3467e13138f5ca57e4a29d2d4bc418738d1
yichehnc <yichenc2017@gmail.com>
2024-05-28 08:30:25 +1000

    feat: a program that will be executed through command line with display message and user input name

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+/* eslint-disable jest/require-hook */
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write('Your name is: ${name}');
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
+});
